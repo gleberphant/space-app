db89ebb9f327f9b92d491ac7134154ca6d81bb4c
Gravatinha <gleberphant@gmail.com>
2026-02-07 18:34:27 -0300

    Auto commit

--- src/componentes/Galeria/Imagem/Imagem.jsx
+++ src/componentes/Galeria/Imagem/Imagem.jsx
@@ -1,7 +1,7 @@
 export default function Imagem({ imagem }) {
     return (
         <div>
-            <p>{imagem.titulo}</p>
+            <p>{imagem.id} {imagem.titulo}</p>
             <img src={imagem.path}></img>
             <p>{imagem.fonte}</p>
         </div>
